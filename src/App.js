ec52bec16c42466f4e0edbaa21ec489bc3c84c5b
Anass NAOUSS <anassnaouss1@gmail.com>
2024-06-25 15:10:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const inputRef= useRef();
+
+  const handleAddTodo =() =>{
+    const text = inputRef.current.value;
+    const newItem ={completed: false, text};
+    setTodos([...todos, newItem])
+    inputRef.current.value="";
+  };
+
+  const handleItemDone = (index) => {
+   const newTodos =[...todos] //creates a copy of the current todos array
+   newTodos[index].completed = !newTodos[index].completed;
+   setTodos(newTodos);  
+  };
+
+  const handleDeleteItem = (index) => {
+    const newTodos =[...todos]
+    newTodos.splice(index, 1 ) //removes one item at the given index
+    setTodos(newTodos) //updates the state with the modified newTodos array
+  }
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <h2>TO DO List</h2>
+     <div className='todo-container'>
+     <ul>
+        {todos.map(({text, completed}, index) => {
+          return(<div className='item'>
+                    <li className={completed ? "done":""} key={index} 
+                        onClick={() => handleItemDone(index)}>  {text}  </li>
+                     <span onClick={() => handleDeleteItem(index)} className='trash'> 🗑️ </span>
+                 </div>);
+        })}
+     </ul> 
+     <input ref={inputRef} placeholder="Enter item..." className='input' />
+     <button onClick={handleAddTodo} className='btn'> Add</button>
+    </div></div>
   );
 }
 
